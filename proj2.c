1484b256cd0234e247405f652be3c662d160f8cc
Petr Mičulek <petr.miculek@gmail.com>
2019-04-04 08:51:29 +0200

    Initial commit
    CFLAGS

---
+++ proj2.c
@@ -0,0 +1,11 @@
+// autor: Petr Miculek xmicul08@stud.fit.vutbr.cz
+// nazev: IOS - Projekt 2
+// datum: odevzdani do 28. 4. 2019
+// vytvareno v: CLion 2018.3, WSL environment, CMake 3.14
+
+#include <stdio.h>
+
+int main() {
+    printf("Hello, Deadlock!\n");
+    return 0;
+}
