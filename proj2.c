db0b202b8caacee396648317e4a930fd1df6da5b
Petr Mičulek <petr.miculek@gmail.com>
2019-04-13 17:07:29 +0200

    Added sys-call checking for parts already written
    nothing tested
    does not build on Wsl

--- proj2.c
+++ proj2.c
@@ -6,6 +6,8 @@
  * 			using processes, semaphores
  * */
 
+/// @TODO Check results of system calls
+
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -15,11 +17,14 @@
 #include <sys/types.h>
 #include <sys/shm.h>
 #include <sys/mman.h>
+#include <sys/stat.h>
 #include <fcntl.h>
 #include <signal.h>
 #include <errno.h>
 #include <string.h>
 #include <stdbool.h>
+
+#include "error.h"
 
 #define barrier_sem_name "/barrier_sem_name"
 #define barrier_mutex_name "/barrier_mutex_name"
@@ -40,8 +45,9 @@ typedef struct {
     sem_t* s_sync_mutex; // init ?
     sem_t* s_hacker_queue; // init ?
     sem_t* s_serf_queue; // init ?
-    bool is_captain;
+    bool is_captain; // @TODO per process
     int *s_shm; // hacker_count, serf_count, isCaptain per process
+    int s_size;
 } sync_t;
 
 /**
@@ -57,8 +63,8 @@ void print_help();
 
 void DEBUG_print_args(int argc, const int *arguments);
 
-void barrier_init(barrier_t *barrier);
-void sync_init(sync_t *barrier);
+int barrier_init(barrier_t *barrier);
+int sync_init(sync_t *sync1);
 
 void barrier_unlink();
 void sync_unlink();
@@ -140,6 +146,7 @@ int parse_int(char *str)
 	char *end_ptr = NULL;
 	long val = strtol(str, &end_ptr, 10);
 
+	errno = 0;
 	if (errno == ERANGE || strlen(str) == 0)
 	{
 		fprintf(stderr, "parsing\n");
@@ -149,7 +156,7 @@ int parse_int(char *str)
 
 	if (val < 0)
 	{
-		fprintf(stderr, "val %s < 0\n", str);
+		warning_msg("parsed value %s < 0\n", str);
 	}
 
 
@@ -157,26 +164,75 @@ int parse_int(char *str)
 	return (int)val;
 }
 
-void barrier_init(barrier_t *barrier)
+int barrier_init(barrier_t *barrier)
 {
     int shmID;
-    shmID = shm_open(barrier_sem_name, O_CREAT | O_EXCL | O_RDWR, S_IRUSR | S_IWUSR);
+    errno = 0;
+    if(!(shmID = shm_open(barrier_sem_name, O_CREAT | O_EXCL | O_RDWR, S_IRUSR | S_IWUSR)))
+    {
+        // shm_open returns -1 on error
+        warning_msg("%s: Error initializing shared mem", "barrier");
+        return -1;
+    }
+
     ftruncate(shmID, sizeof(int));
-    barrier->b_shm = (int*)mmap(NULL, sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED, shmID, 0);
+    errno = 0;
+    if((barrier->b_shm = (int*)mmap(NULL, sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED, shmID, 0)) == MAP_FAILED)
+    {
+        warning_msg("%s: Error mapping shared mem", "barrier");
+        return -1;
+    }
+
     close(shmID);
-    *(barrier->b_shm) = 0;
+    *(barrier->b_shm) = 0; // @TODO why?
     munmap(barrier->b_shm, sizeof(int));
 
-    barrier->b_mutex = sem_open(barrier_mutex_name, O_CREAT, 0666, 1);
-    barrier->b_sem = sem_open(barrier_sem_name, O_CREAT, 0666, 0);
+    errno = 0;
+    if((barrier->b_mutex = sem_open(barrier_mutex_name, O_CREAT, 0666, 1)) == SEM_FAILED)
+    {
+        warning_msg("%s,%s: Error initializing semaphore", "barrier", "1");
+        return -1;
+    }
+
+    errno = 0;
+    if((barrier->b_sem = sem_open(barrier_sem_name, O_CREAT, 0666, 0)) == SEM_FAILED)
+    {
+        warning_msg("%s,%s: Error initializing semaphore", "barrier", "2");
+        return -1;
+    }
+
+
     sem_close(barrier->b_mutex);
     sem_close(barrier->b_sem);
+    return 0;
 }
 
-void sync_init(sync_t *sync1)
+int sync_init(sync_t *sync1)
 {
-    (void)sync1;
+    int sync_shmID;
+    if(!(sync_shmID = shm_open(sync_sem_name, O_CREAT | O_EXCL | O_RDWR, S_IRUSR))){
+        // shm_open returns -1 on error
+        warning_msg("%s: Error initializing shared mem", "sync");
+        return -1;
+    }
 
+    ftruncate(sync_shmID, sizeof(int));
+
+    errno = 0;
+    if((sync1->s_shm = (int*)mmap(NULL, sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED, sync_shmID, 0)) == MAP_FAILED) {
+        warning_msg("%s: Error mapping shared mem", "sync");
+        return -1;
+    }
+
+    close(sync_shmID);
+
+    if(!(munmap(sync1->s_shm, sizeof(int)))) {
+        warning_msg("%s: Error un-mapping shared mem", "sync");
+        return -1;
+    }
+
+
+    return 0;
 }
 
 
